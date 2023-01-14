7151b1e44b53b4e64f64d59ec3043e5d5390a2c1
Denisio Rordrigues <denisio@ymail.com>
2023-01-14 17:45:04 -0300

    Desafio completo

---
+++ README.md
@@ -0,0 +1,11 @@
+## Desafio da API do Pokemon
+
+Documentação: 
+https://pokeapi.co/docs/v2#pokemon-section
+
+Link pra buscar o pokemon:
+https://pokeapi.co/api/v2/pokemon/1
+
+
+Feito apartir do vídeo
+https://www.youtube.com/watch?v=CnWRFjt5t-Q
--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,69 @@ EXTRA: se puder ordene por nome.
 */
 
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 
 function App() {
-  useEffect(() => {
+
+  const [list, setList] = useState([])
+
+  const fetchListData = () =>{
     axios
-    .get('https://pokeapi.co/api/v2/pokemon')
-    .then(response => {
-      console.log(response);
+    .get('https://pokeapi.co/api/v2/pokemon').then(response => {
+      const sortedArray = [...response.data.results];
+
+      sortedArray.sort((a,b) => {
+        return a.name.localeCompare(b.name);
+      });
+
+      setList(sortedArray);
     })
+  };
+
+  useEffect(() => {
+    fetchListData()
   }, []);
 
   return (
     <div className="App">
       <h3>Desafio React</h3>
       <h1>Consumir API Pokemon</h1>
+      <hr/>
+      {
+        list.map(item => <Pokemon key={item.name} data={item} />)
+      }
     </div>
   )
 }
+
+
+const Pokemon = ({data}) => {
+  const [details, setDetails] = useState(null);
+  
+  const fetchIndividualPokemon = ()=> {
+    axios.get(data.url).then((response) => setDetails(response.data));
+  }
+  
+  useEffect(() => {
+    fetchIndividualPokemon();
+  }, [])
+
+  if(details === null) {
+    return <div>Carregando...</div>
+  }
+
+  return <>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <span>
+        <img src={details.sprites.front_default} style={{ width: 50, marginRight: 20}}></img>
+      </span>
+      <span>
+        <b>{details.name.toUpperCase()}</b>
+      - EXP {details.base_experience}
+      </span>
+      </div>
+  </>;
+};
 
 export default App
